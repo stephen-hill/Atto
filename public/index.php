af0b3662ac88b25a8d7656c4bb2480daca2c00d6
Stephen Hill <stephen@gatekiller.co.uk>
2015-04-01 16:58:59 +0100

    Loading the request class.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 require('../vendor/autoload.php');
+
+$request = new Atto\Request([
+    'post' => $_POST,
+    'query' => $_GET,
+    'server' => $_SERVER,
+    'files' => $_FILES,
+    'cookies' => $_COOKIES
+]);
+
 (new Atto\App())->run();
