db99106159e103e2d69b907672918a997e5d01eb
Stephen Hill <stephen@gatekiller.co.uk>
2015-05-11 16:43:25 +0100

    Added license and documentation.

--- src/Config.php
+++ src/Config.php
@@ -1,34 +1,74 @@
 <?php
 
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Stephen Hill
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 namespace Atto;
 
 use Exception;
 use InvalidArgumentException;
 
+/**
+ * Class for reading values from JSON configuration files.
+ *
+ * @author Stephen Hill stephen@gatekiller.co.uk
+ * @license http://opensource.org/licenses/MIT MIT
+ */
 class Config
 {
     /**
+     * Full or relative path of the config file.
+     *
      * @var string
      */
     protected $path;
 
     /**
+     * Changed to TRUE is the config file has been loaded to memory.
+     *
      * @var boolean
      */
     protected $loaded = false;
 
     /**
-     * @var array|null
+     * The JSON data converted into an array.
+     *
+     * @var array
      */
     protected $data;
 
     /**
+     * Cache for ->get($key) calls.
+     *
      * @var array
      */
     protected $cache = [];
 
     /**
      * @param string $path Full or relative path of the config file.
+     * @throws InvalidArgumentException Throws an InvalidArgumentException if
+     *                                  the $path argument is not a string.
      */
     public function __construct($path)
     {
@@ -41,6 +81,16 @@ class Config
         $this->path = $path;
     }
 
+    /**
+     * Get the value of a configuration key.
+     * Nested keys can be specified in dot notation.
+     *
+     * @param string $key The string of the key.
+     * @param mixed $default This is returned if the key does not exist.
+     * @return mixed Returns the value the key holds.
+     * @throws Exception Throws an exception is the key does not exist
+     *                   and a default is not defined.
+     */
     public function get($key, $default = null)
     {
         // Check the cache
@@ -54,7 +104,7 @@ class Config
         {
             $this->load();
         }
-        
+
         // Split the key segments into an array
         $segments = explode('.', $key);
         $data = $this->data;
@@ -69,7 +119,7 @@ class Config
                 continue;
             }
         }
-        
+
         if ($value !== null)
         {
             return $this->cache[$key] = $value;
@@ -83,6 +133,12 @@ class Config
         return $default;
     }
 
+    /**
+     * Loads the config file from disk and converts the JSON to an array.
+     * @return void
+     * @throws Exception Throws an Exception is the config file does not exist.
+     * @throws Exception Throws an Exception if there is a JSON parsing error.
+     */
     protected function load()
     {
         // Check if the config file exists
